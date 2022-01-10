3e564ed63536ddd30f04a615365f896449120ef4
Helmut Irle <helmut@devigninnovation.com>
2022-01-10 07:23:11 +0200

    Added image and file models.

--- things/admin.py
+++ things/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from things.models import Thing
+from things.models import Thing, File, Image
 
 
 @register(Thing)
 class ThingAdmin(admin.ModelAdmin):
+    pass
+
+
+@register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+@register(File)
+class FileAdmin(admin.ModelAdmin):
     pass
--- things/models.py
+++ things/models.py
@@ -6,3 +6,13 @@ class Thing(models.Model):
     description = models.TextField(null=False, blank=False)
     updated_at = models.DateTimeField(null=True)
     thingiverse_id = models.IntegerField(null=False)
+
+
+class Image(models.Model):
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE)
+    source_url = models.URLField(null=False)
+
+
+class File(models.Model):
+    thing = models.ForeignKey(Thing, on_delete=models.CASCADE)
+    source_url = models.URLField(null=False)
