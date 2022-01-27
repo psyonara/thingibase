0a792b90e1674c5f3873c4a9591da603ee49de49
Helmut Irle <helmut@devigninnovation.com>
2022-01-27 07:03:48 +0200

    Scraped image and file URLs.

--- things/fetch.py
+++ things/fetch.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 from django.conf import settings
 from gazpacho import Soup
@@ -24,18 +26,19 @@ def get_html(url):
             f"There was a problem with the request ({resp.status_code}): {resp.content.decode()}"
         )
 
-    return resp.content.decode()
+    json_response = json.loads(resp.content.decode())
+    return json_response["content"]
 
 
 def get_thing_soup(thing_id):
     """
     Get the page of a thing from Thingiverse.
     :param thing_id: ID of thing on Thingiverse
-    :return: The 'Soup' of the page
+    :return: The 'Soup' of the page with summary, and the 'Soup' of the page with downloads
     """
-    url = f"https://www.thingiverse.com/thing:{thing_id}"
-    html = get_html(url)
-    return Soup(html)
+    base_url = f"https://www.thingiverse.com/thing:{thing_id}"
+    downloads_url = f"https://www.thingiverse.com/thing:{thing_id}/files"
+    return Soup(get_html(base_url)), Soup(get_html(downloads_url))
 
 
 def get_thing_details(thing_id):
@@ -44,15 +47,33 @@ def get_thing_details(thing_id):
     :param thing_id: ID of thing on Thingiverse
     :return: A dict containing details of the thing
     """
-    soup = get_thing_soup(thing_id)
-    title = soup.find("div", {"class": "ThingPage__modelName"}, mode="first").html
+    soup, downloads_soup = get_thing_soup(thing_id)
+    title = soup.find("div", {"class": "ThingPage__modelName"}, mode="first").text
     summary = soup.find("div", {"class": "ThingPage__description"}, mode="first").html
     print_settings = soup.find("div", {"class": "ThingPage__preHistory"}, mode="all")
 
     print_parts = "".join(part.html for part in print_settings)
     description = f"{summary}{print_parts}"
 
+    image_carousel = soup.find("div", {"class": "ThingImageCarousel__carouselWrapper"}, mode="first")
+    image_items = image_carousel.find("li", {"class": "slide"})
+    images = []
+    for item in image_items:
+        img_src = item.find("img").attrs["src"]
+        if img_src.find("img.youtube.com") > -1:
+            iframe_src = item.find("iframe").attrs["src"]
+            if iframe_src.startswith("//www"):
+                iframe_src = iframe_src.replace("//www", "https://www")
+            images.append(iframe_src)
+        else:
+            images.append(img_src)
+
+    file_links = downloads_soup.find("a", {"class": "ThingFile__download"}, mode="all")
+    files = [link.attrs["href"] for link in file_links]
+
     return {
         "title": title,
-        "description": description
+        "description": description,
+        "images": images,
+        "files": files,
     }
